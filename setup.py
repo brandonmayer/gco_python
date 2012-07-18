c1cd4c34b18df4caefc02517000ca44804afc94f
bm <bm@128.148.16.131>
2012-07-17 22:56:08 -0400

    added os for cross-platform file names

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
+import os
 
-gco_directory = "../../tools/gco/"
+#gco_directory = "../../tools/gco/"
+gco_directory = "C:/gc0-v3.0/src"
+gco_directory = os.path.realpath(gco_directory) 
 
 files = ['GCoptimization.cpp', 'graph.cpp', 'LinkedBlockList.cpp',
     'maxflow.cpp']
 
-files = [gco_directory + f for f in files]
+files = [os.path.join(gco_directory,f) for f in files]
 files.insert(0, "gco_python.pyx")
 
 setup(cmdclass={'build_ext': build_ext},
